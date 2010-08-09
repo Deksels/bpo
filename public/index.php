a8a5356a2fa83c4bcfbb065dd92375548f6106fd
jarra <jarra@jarra.nl>
2010-08-09 21:35:24 +0200

    Initial commit.

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xml:lang="nl-NL" xmlns="http://www.w3.org/1999/xhtml" lang="nl-NL"><head>
+
+
+
+
+
+
+<title>Bad Pixel Out</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="author" content="Bad Pixel Out">
+	<meta name="keywords" content="bart,baekelandt,jarra,schirris">
+	<meta name="description" content="Bad Pixel Out">
+	<meta name="robots" content="noarchive">
+	<meta name="robots" content="index,follow">
+	
+</head><body style="background-color: black;">
+	<p style="font: bold 60px/1em Helvetica,sans-serif; color: white; margin: 50px 0pt 0pt 50px;">BadPixelOut</p>
+</body></html>

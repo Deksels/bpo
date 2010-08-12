75bbaf731717ffaf29f5e1fd6975e6ee895143af
Bart <mail@bartbaekeland.com>
2010-08-12 22:08:39 +0200

    added message on homepage

--- public/index.php
+++ public/index.php
@@ -1,7 +1,6 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xml:lang="nl-NL" xmlns="http://www.w3.org/1999/xhtml" lang="nl-NL">
 <head>
-
   <title>Bad Pixel Out</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<meta name="author" content="Bad Pixel Out">
@@ -12,10 +11,10 @@
 
   <script src="js/prototype.s2.min.js" type="text/javascript"></script>
   <script src="js/home.js" type="text/javascript"></script>
-	
 </head>
 
 <body id="body" style="background-color: #000;">
 	<p id="p" style="font: bold 60px/1em Helvetica,sans-serif; color: white; margin: 50px 0pt 0pt 50px;">BadPixelOut</p>
+	<p>Let us show you what makes the web great!</p>
 </body>
 </html>
